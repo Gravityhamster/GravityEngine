ab0c4c5ecb6285b39d1d22a01be405aa0364af0c
Gravityhamster <bluehamster648@gmail.com>
2026-02-09 00:41:45 -0500

    Include complex debug

--- GravityEngine/Example_Boiler.cpp
+++ GravityEngine/Example_Boiler.cpp
@@ -164,7 +164,7 @@ int main()
     GravityEngine_Core ge_inst = GravityEngine_Core("Boiler Plate", "com.example.snake", "1.0", 96, 54, 60, 1920, 1080, "./Ubuntu-B-1.ttf", 16);
 
     ge_inst.debug_mode = true; // Show debug overlay
-    ge_inst.debug_complex = false; // Show all information
+    ge_inst.debug_complex = true; // Show all information
     geptr = &ge_inst; // Set the pointer to the console engine class
 
     // Start game loop
