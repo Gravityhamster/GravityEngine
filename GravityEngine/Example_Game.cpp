6a980cc64fe0ff20dded16a23b90d817e0396c13
Gravityhamster <bluehamster648@gmail.com>
2026-06-23 18:56:56 -0400

    Draw the chain list for the song

--- GravityEngine/Example_Game.cpp
+++ GravityEngine/Example_Game.cpp
@@ -10,13 +10,27 @@ int bpm = 60; // Beats per minute
 int fps = 360; // Frame rate in hz
 int tps = 6; // Ticks per step
 double frametick = 0;
+int songgrid[0xffff][64];
 
+// Tracker colors
 color primary_text_a = { {255, 255, 255}, {0, 0, 0} };
 color header_text_a = { {255, 255, 255}, {0, 0, 100} };
 color primary_text_b = { {0, 0, 0}, {255, 255, 255} };
 color body_text_a = { {255, 255, 255}, {0, 0, 0} };
 color body_text_b = { {0, 0, 0}, {255, 255, 255} };
 
+// Song editor menu
+enum menu
+{
+    song,
+    chain,
+    phrase,
+    instrument
+};
+
+menu state = song;
+
+// TODO: Gravityengine objects if necessary
 class player : public virtual GravityEngine_Object
 {
     private:
@@ -58,6 +72,7 @@ double BpmToFrametick(int b, int f)
 // Execute tick
 void DoTick()
 {
+    // TODO: Handle playing the music
     // geptr->DrawTextString(5, 6, geptr->entity, std::to_string(synptr2->freq), { {255,255,255},{0,0,0} });
     synptr2->volume = 0.25;
 }
@@ -68,16 +83,20 @@ bool str_contains(std::string s, std::string f) { return s.find(f) != std::strin
 // Convert i to hex string
 std::string IntToHexString(int i)
 {
+    // Write the int to a stream as hex
     std::ostringstream hexs;
     hexs << std::hex << i;
 
+    // Get the string from the stream
     std::string tempstr = hexs.str();
     std::string upperstr = "";
 
     // https://www.geeksforgeeks.org/cpp/toupper-in-cpp/
+    // To upper
     for (auto x : tempstr)
         upperstr += (char)toupper(x);
 
+    // Return
     return upperstr;
 }
 
@@ -87,13 +106,19 @@ std::string IntToHexString(int i)
 // type : Draw type (What do you want to redraw?) [all, title, x, y, navigator]
 void DrawSongUI(int off_x, int off_y, std::string type)
 {
+    // Menu title
     if (str_contains(type, "all") || str_contains(type, "title"))
     {
         geptr->DrawTextString(0, 1, geptr->background, "SONG", primary_text_a);
     }
+
+    // Row numbers
     if (str_contains(type, "all") || str_contains(type, "y"))
     {
+        // Height of the screen
         int h = geptr->GetCanvasH() - 4;
+
+        // Draw the row numbers
         for (int i = 0; i < h; i++)
         {
             int tempint = i + off_y;
@@ -104,9 +129,14 @@ void DrawSongUI(int off_x, int off_y, std::string type)
             geptr->DrawTextString(0, 3 + i, geptr->background, upperstr, primary_text_a);
         }
     }
+
+    // Channel headers
     if (str_contains(type, "all") || str_contains(type, "x"))
     {
+        // Width of the screen
         int w = (geptr->GetCanvasW() - 5) / 5;
+
+        // Draw the headers
         for (int i = 0; i < w; i++)
         {
             std::string tempstr = std::to_string(i + off_x);
@@ -114,23 +144,53 @@ void DrawSongUI(int off_x, int off_y, std::string type)
             geptr->DrawTextString(5 + i*5, 2, geptr->background, "CH" + tempstr, header_text_a);
         }
     }
+
+    // Song grid navigator
     if (str_contains(type, "all") || str_contains(type, "navigator"))
     {
+        // Width and height of the screen
         int h = geptr->GetCanvasH() - 4;
         int w = (geptr->GetCanvasW() - 5) / 5;
 
-        // TODO: Draw the song map
+        //Draw the song grid
+        for (int y = 0; y < h; y++)
+        {
+            for (int x = 0; x < w; x++)
+            {
+                int chain = songgrid[y + off_y][x + off_x];
+                if (chain == -1)
+                {
+                    geptr->DrawTextString(5 + x * 5, 3 + y, geptr->background, "----", primary_text_a);
+                }
+                else
+                {
+                    auto outstr = IntToHexString(chain);
+                    outstr.insert(outstr.begin(), 4 - outstr.size(), '0');
+                    geptr->DrawTextString(5 + x * 5, 3 + y, geptr->background, outstr, primary_text_a);
+                }
+            }
+        }
     }
 }
 
 // Master pre code
 void GameInit()
 {
+    // Init song phrase list
+    for (int y = 0; y < 0xffff; y++)
+    {
+        for (int x = 0; x < 64; x++)
+        {
+            songgrid[y][x] = -1;
+        }
+    }
+
     // Start song UI
     DrawSongUI(0, 0, "all");
 
+    // Test: Init synth and play it
     synptr2 = new GravityEngine_Synth();
-    //synptr2->pulse_width_freq = 0.5f;
+    synptr2->pulse_width_freq = 0.5f;
     synptr2->freq = 261.63;
     synptr2->volume = 0.25;
     synptr2->volume_freq = -10;
